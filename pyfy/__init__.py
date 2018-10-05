39b0e58e76658889910aa12642d91af0107276dc
Omar Ryhan <omarryhan@gmail.com>
2018-10-05 11:59:06 +0200

    Fixed project URL

--- pyfy/__init__.py
+++ pyfy/__init__.py
@@ -3,7 +3,7 @@ from .client import ClientCreds, UserCreds, Spotify, AuthError, ApiError, Spotif
 
 __name__ = 'pyfy'
 __about__ = "Lightweight python wrapper for Spotify's web API"
-__url__ = 'https://github.com/omarryhan/spyfy'
+__url__ = 'https://github.com/omarryhan/pyfy'
 __version_info__ = ('0', '0', '12')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Omar Ryhan'
