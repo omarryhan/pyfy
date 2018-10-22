19c14ed1dc314931212736c4c41149e2486eb045
Omar Ryhan <omarryhan@gmail.com>
2018-10-22 23:20:44 +0200

    Bumped version for scopes hotfix

--- pyfy/__init__.py
+++ pyfy/__init__.py
@@ -4,7 +4,7 @@ from .client import ClientCreds, UserCreds, Spotify, AuthError, ApiError, Spotif
 __name__ = 'pyfy'
 __about__ = "Lightweight python wrapper for Spotify's web API"
 __url__ = 'https://github.com/omarryhan/pyfy'
-__version_info__ = ('0', '0', '12')
+__version_info__ = ('0', '0', '13')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Omar Ryhan'
 __author_email__ = 'omarryhan@gmail.com'
