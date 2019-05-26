fe1e34ec414fcb2e9bade1fde1a261b2f7d5a105
Omar Ryhan <omarryhan@gmail.com>
2019-05-26 13:45:13 +0200

    Bumped version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Omar Ryhan"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "2.0.1"
 
 
 # -- General configuration ---------------------------------------------------
