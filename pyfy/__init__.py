edc72cefe887f517b3f0d0eb71e89f90b9b6bb9b
Omar Ryhan <omarryhan@gmail.com>
2019-10-30 17:14:58 +0200

    Imported __version__ in __init__

--- pyfy/__init__.py
+++ pyfy/__init__.py
@@ -1,5 +1,17 @@
-from .sync_client import Spotify  # noqa:  F401
-from .async_client import AsyncSpotify  # noqa:  F401
-from .utils import convert_from_iso_date  # noqa:  F401
-from .creds import ClientCreds, UserCreds, ALL_SCOPES  # noqa:  F401
-from .excs import ApiError, SpotifyError, AuthError  # noqa:  F401
+from .__version__ import (  # noqa: F401  imported but unused
+    __name__,
+    __about__,
+    __url__,
+    __version_info__,
+    __version__,
+    __author__,
+    __author_email__,
+    __maintainer__,
+    __license__,
+    __copyright__,
+)
+from .sync_client import Spotify  # noqa:  F401  imported but unused
+from .async_client import AsyncSpotify  # noqa:  F401  imported but unused
+from .utils import convert_from_iso_date  # noqa:  F401  imported but unused
+from .creds import ClientCreds, UserCreds, ALL_SCOPES  # noqa:  F401  imported but unused
+from .excs import ApiError, SpotifyError, AuthError  # noqa:  F401  imported but unused
