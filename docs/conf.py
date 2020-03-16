d2ee81edcb4b749cc925f61999a0d9b74ec76a50
Omar Ryhan <omarryhan@gmail.com>
2020-03-16 17:06:58 +0200

    Bumped version and ready for new Pip release

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Omar Ryhan"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "2.0.5"
+release = "2.0.6"
 
 
 # -- General configuration ---------------------------------------------------
